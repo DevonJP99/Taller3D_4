c3685adb5ea4867e815d103a73df1c4e75bd8608
LeonardoACM <107737131+LeonardoACM@users.noreply.github.com>
2022-10-29 00:11:58 -0500

    Armas Part1

---
+++ Taller 3d.u/Assets/Scriptsaaa/Guns.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guns : MonoBehaviour
+{
+    public int damage;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+    //reference
+    public Camera fpcCam;
+    public Transform attacPoint;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        MyInput();
+    }
+
+    private void MyInput()
+    {
+        if (allowButtonHold)
+        {
+            shooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else
+        {
+            shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+    }
+}

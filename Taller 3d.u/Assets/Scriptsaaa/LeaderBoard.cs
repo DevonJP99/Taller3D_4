52fe83b9673e3e586e4fa1aed5fd4a6461430e79
DevonJP <105997401+DevonJP@users.noreply.github.com>
2022-11-25 19:02:09 -0500

    cuadros texto

--- Taller 3d.u/Assets/Scriptsaaa/LeaderBoard.cs
+++ Taller 3d.u/Assets/Scriptsaaa/LeaderBoard.cs
@@ -20,12 +20,71 @@ public class LeaderBoard : MonoBehaviour
     public InputField score;
     public InputField display;
 
+    List<Playerinfo> collectedstats;
+
     private void Start()
     {
-        
+        collectedstats = new List<Playerinfo>();
+        LoadLeaderBoard();
     }
     public void SumbitButton()
     {
+        //create object using values from imputfields, is done so that a name and score be moved 
+        Playerinfo stats = new Playerinfo(userName.text, int.Parse(score.text));
+        collectedstats.Add(stats);
+        userName.text = "";
+        score.text = "";
+        SortStats();
+    }
+    void SortStats()
+    {
+        for(int i = collectedstats.Count-1;i>0;i++)
+        {
+            if(collectedstats[i].score>collectedstats[i-1].score)
+            {
+                Playerinfo tempinfo = collectedstats[i - 1];
+                collectedstats[i - 1] = collectedstats[i];
+                collectedstats[i] = tempinfo;
+            }
+        }
+        UpdatePlayerPrefsString();
+    }
+    void UpdatePlayerPrefsString()
+    {
+        string stats = "";
+        for(int i=0;i<collectedstats.Count;i++)
+        {
+            stats += collectedstats[i].name + ",";
+            stats += collectedstats[i].score + ",";
+        }
+        PlayerPrefs.SetString("LeaderBoards", stats);
+        UpdateLeaderBoardVisual();
+    }
+    void UpdateLeaderBoardVisual()
+    {
+        display.text = "";
+        for (int i = 0; i < collectedstats.Count - 1; i++)
+        {
+            display.text += collectedstats[i].name + ":" + collectedstats[i].score + "/n";
+        }
+    }
+    void LoadLeaderBoard()
+    {
+        string stats = PlayerPrefs.GetString("LeaderBoards", "");
 
+        string[] stats2 = stats.Split(',');
+
+        for(int i=0; i<stats2.Length-2;i+=2)
+        {
+            Playerinfo loadedinfo = new Playerinfo(stats2[i], int.Parse(stats2[i + 1]));
+
+            collectedstats.Add(loadedinfo);
+            UpdateLeaderBoardVisual();
+        }
+    }
+    public void ClearPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        display.text = "";
     }
 }
---
+++ Taller 3d.u/Assets/Scriptsaaa/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timero;
+    public TextMeshProUGUI timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GameObject.Find("temporizador").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timero -= Time.deltaTime;
+        timer.text = timero.ToString("f0");
+        if(timero==0)
+        {
+            ScoreScene();
+        }
+    }
+    public void ScoreScene()
+    {
+        SceneManager.LoadScene("LeaderBoard");
+    }
+}

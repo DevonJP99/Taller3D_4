f635b352adbdffaf3c0e382f3a8d99656d19cb6b
Sebastian <>
2022-12-02 04:01:06 -0500

    comprador ahce wiii

--- Taller 3d.u/Assets/Enemies/Scripts/Comprador/CompradorStateManager.cs
+++ Taller 3d.u/Assets/Enemies/Scripts/Comprador/CompradorStateManager.cs
@@ -7,14 +7,25 @@ public class CompradorStateManager : EnemyBaseStateMachine
 {
     [SerializeField]
     Collider cart;
+    float force = 50;
+    float horizontal_force = 200;
 
     public void ReceiveDamage(int damage_received, PlayerStaticVariable player)
     {
         //aniamtion destroy
         //OnReceiveDamage?.Invoke(damage_received, player);
-        Destroy(gameObject);
+        Destroy(gameObject, 1);
         //unables ai, gravity
         //actives traigger or no collider
         // force
+        GetComponent<NavMeshAgent>().isStopped = true;
+        GetComponent<NavMeshAgent>().enabled = false;
+        cart.enabled = false;
+        self.enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.up * horizontal_force;
+        //GetComponent<Rigidbody>().velocity = (transform.position - player.transform.position).normalized * force; 
+        GetComponent<Rigidbody>().AddForce((transform.position - player.transform.position).normalized * force, ForceMode.Impulse); 
     }
 }

17fb89352bd8f40bca627b1dcba10ec66a969367
LeonardoACM <107737131+LeonardoACM@users.noreply.github.com>
2022-12-08 13:35:44 -0500

    Opciones

--- Taller 3d.u/Assets/Scriptsaaa/SceneManagement.cs
+++ Taller 3d.u/Assets/Scriptsaaa/SceneManagement.cs
@@ -1,27 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagement : MonoBehaviour
 {
+    [Header("opciones")]
+    public Slider brillo;
+    public Toggle pantalla;
+    public Slider musica;
+    public Slider volumefx;
+    public AudioMixer mixed;
+    public AudioSource fxsource;
+    public AudioClip clipsound;
+
+    [Header("paneles")]
+    public GameObject panelprincipal;
+    public GameObject panelopciones;
+    public GameObject panelpuntaje;
+    public GameObject panelsalida;
+
+    private void Awake()
+    {
+        volumefx.onValueChanged.AddListener(changevolumeFX);
+        musica.onValueChanged.AddListener(changevolumeMaster);
+    }
+    public void openpanel(GameObject panel)
+    {
+        panelprincipal.SetActive(false);
+        panelopciones.SetActive(false);
+        panelpuntaje.SetActive(false);
+        panelsalida.SetActive(false);
+
+        panel.SetActive(true);
+        playsound();
+    }
+
+    public void changevolumeMaster(float v)
+    {
+        mixed.SetFloat("VolMaster",v);
+    }
+
+    public void changevolumeFX(float v)
+    {
+        mixed.SetFloat("VolFX", v);
+    }
+
+    public void playsound()
+    {
+        fxsource.PlayOneShot(clipsound);
+    }
+
+
     public void loadgame()
     {
         SceneManager.LoadScene("Zona de Pruebas");
-    }
-
-    public void loadOptions()
-    {
-        SceneManager.LoadScene("Opciones");
-    }
-
-    public void loadLeaderBoard()
-    {
-        SceneManager.LoadScene("Puntaje");
-    }
-    public void loadMarket()
-    {
-        SceneManager.LoadScene("Market");
     }
 
     public void loadcredits()
@@ -33,24 +68,14 @@ public class SceneManagement : MonoBehaviour
     {
         SceneManager.LoadScene("Menu");
     }
-    public void loadexit()
-    {
-        SceneManager.LoadScene("Salir");
-    }
 
     public void salir()
     {
         Application.Quit();
     }
 
-    public void AumentarCoins()
-    {
-        MejorasStatic.coins = MejorasStatic.coins + 5;
-    }
     public void tuto()
     {
         SceneManager.LoadScene("Tutorial");
     }
-    
-  
 }
